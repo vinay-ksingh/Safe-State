b012965063869e501fd520129b37ece995d1ecfc
vs84341 <49991778+vs84341@users.noreply.github.com>
2019-04-25 23:00:01 +0530

    Update vinay.c

--- vinay.c
+++ vinay.c
@@ -1,3 +1,9 @@
+/*
+Consider that a system has P resources of same type. These resources are shared by Q processes time to time. All processes request and release the resources one at a time. Generate a solution to demonstrate that, the system is in safe state when following conditions are satisfied.
+Conditions:
+a.) Maximum resource need of each process is between 1 and P.		                 
+b.) Summation of all maximum needs is less than P+Q
+*/
 #include<stdio.h>
 #include<conio.h>
 #include<string.h>
