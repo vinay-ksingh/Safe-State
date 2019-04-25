499cb97d8c709b32d6e316e4583d4fc2fd39f4ca
vs84341 <49991778+vs84341@users.noreply.github.com>
2019-04-25 22:57:24 +0530

    Add files via upload

---
+++ vinay.c
@@ -0,0 +1,84 @@
+#include<stdio.h>
+#include<conio.h>
+#include<string.h>
+void main()
+{
+	
+int q,p,smax,sflag=0;
+int i=0,j,flag[10],n=0,kill=0,sum=0,available=0;
+int ui[10];
+int max[10],alloc[10],need[10];
+printf("Enter the no of processes:"); 
+scanf("%d",&q);
+printf("Enter the no of resources:");
+scanf("%d",&p);
+smax=p+q;
+
+for(i=0;i<q;i++)
+{
+if(i==0)
+{
+max[i]=p-1;
+}
+else
+{
+max[i]=1;
+}
+}
+
+for(i=0;i<q;i++)
+{
+if(i==0)
+{
+alloc[i]=p-2; 
+}
+else
+{alloc[i]=0;}
+}
+
+for(i=0;i<q;i++)
+{
+sum=alloc[i]+sum;
+}
+available=q-sum;
+
+for(i=0;i<q;i++)
+{
+need[i]=max[i]-alloc[i];
+}
+
+while(n<=q)
+{
+for(i=0;i<q;i++)
+{
+if(need[i]<available)
+{
+available=available+need[i];
+flag[i]=1;
+n=n+1;
+}
+
+else
+{flag[i]=0;}
+}
+for(i=0;i<q;i++)
+{
+if(flag[i]==1)
+sflag=sflag+1;
+}
+if(sflag==0)
+{
+n=q+1;
+kill=1;
+}
+}
+
+if(kill==1){printf("Unsafe or Deadlock occured");}
+
+else
+{
+	printf("Safe State");
+}
+}
+
+ 
