b7821ca1aa8de137aee1fc538e173c2156155510
vs84341 <49991778+vs84341@users.noreply.github.com>
2019-04-25 23:30:49 +0530

    Update vinay.c

--- vinay.c
+++ vinay.c
@@ -1,5 +1,7 @@
 /*
-Consider that a system has P resources of same type. These resources are shared by Q processes time to time. All processes request and release the resources one at a time. Generate a solution to demonstrate that, the system is in safe state when following conditions are satisfied.
+Consider that a system has P resources of same type. These resources are shared by Q processes time to time. 
+All processes request and release the resources one at a time. Generate a solution to demonstrate that, the system is in safe state 
+when following conditions are satisfied.
 Conditions:
 a.) Maximum resource need of each process is between 1 and P.		                 
 b.) Summation of all maximum needs is less than P+Q
@@ -47,7 +49,6 @@ for(i=0;i<q;i++)
 sum=alloc[i]+sum;
 }
 available=q-sum;
-
 for(i=0;i<q;i++)
 {
 need[i]=max[i]-alloc[i];
